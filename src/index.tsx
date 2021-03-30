7b9fbe8cde551a1f0f934f600c6b03a7555a7381
bojoluis21798 <bojoluis21798@gmail.com>
2021-03-30 12:47:37 +0800

    Apply global styles and provide theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { theme, GlobalStyle } from "./styles";
 
-render(<App />, document.getElementById("app"));
+render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>,
+  document.getElementById("app")
+);
---
+++ src/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
+export const theme = {
+  headingFont: "500 1rem 'Montserrat', sans-serif",
+  headingFontHeavy: "600 1rem 'Montserrat', sans-serif",
+  white: "#fff",
+  accent: "#5692e8",
+  textColor: "#c1d1e8",
+  cardColor: "#0e1a2b",
+  inputColor: "#182c47",
+};
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    font: 500 12px 'Mulish', sans-serif;
+    background-image: linear-gradient(#081221, #03080f);
+    color: ${theme.textColor};
+  }
+
+  button {
+    background-color: ${theme.accent};
+  }
+
+  h1, h2, h3, h4, h5, h6, button, label {
+    color: ${theme.white}
+  }
+`;
